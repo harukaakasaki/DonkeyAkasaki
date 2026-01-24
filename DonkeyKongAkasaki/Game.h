e9ac7038b848ecd0372a69daeb0ee73a0178a51f
harukaakasaki <harukajobstar@gmail.com>
2026-01-24 22:53:31 +0900

    更新

--- DonkeyKongAkasaki/Game.h
+++ DonkeyKongAkasaki/Game.h
@@ -4,7 +4,7 @@
 namespace Game
 {
 	// ‰æ–Êî•ñ
-	constexpr int kScreenWidth = 1980;
-	constexpr int kScreenHeight = 1080;
+	constexpr int kScreenWidth = 1600;
+	constexpr int kScreenHeight = 900;
 	constexpr int kColorBitNum = 32;
 }
